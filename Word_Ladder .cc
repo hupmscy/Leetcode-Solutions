6b1b35843fadca80802f4e6652d3c0615182466d
hupmscy <galaxysf3@gmail.com>
2013-12-14 14:49:13 +0800

    commit

--- Word_Ladder .cc
+++ Word_Ladder .cc
@@ -1,32 +1,96 @@
 class Solution {
 public:
-    #define SIZE 5000
-    bool dp[SIZE];
-    bool wordBreak(string s, unordered_set<string> &dict) {
-        if (s.size() == 0 || dict.size() == 0)
-            return false;
-        int max_len = -1;
-        int min_len = INT_MAX;
-        memset(dp, false, sizeof(dp));
-        for (unordered_set<string>::iterator iter = dict.begin(); iter != dict.end(); iter++)
+
+    #define SIZE 10000
+
+    struct E
+    {
+        int t, next;
+    };
+
+    E edge[SIZE*4];
+    int head[SIZE], pos;
+    bool visit[SIZE];
+    string str[SIZE];
+    bool has_edge(const char *a, const char *b, int len)
+    {
+        int diff = 0;
+        for (int i = 0; i < len; i++)
+            if (a[i] != b[i])
+            {
+                diff++;
+                if (diff > 1)
+                    return false;
+            }
+        return diff ? true : false;
+    }
+
+    void add_edge(int a, int b)
+    {
+        edge[pos].next = head[a];
+        edge[pos].t = b;
+        head[a] = pos++;
+    }
+
+    int bfs(int s, int t)
+    {
+        queue<pair<int, int>> q;
+        pair<int, int> tmp;
+        int cur, step;
+        visit[s] = true;
+        q.push(make_pair(s, 1));
+        while (!q.empty())
         {
-            int size = (*iter).size();
-            if (size > max_len)
-                max_len = size;
-            if (size < min_len)
-                min_len = size;
-        }
-        dp[0] = true;
-        for (int i = min_len; i <= s.size(); i++)
-        {
-            int bound = i-max_len >= 0 ? i-max_len:0;
-            for (int j = i-min_len; j >= bound; j--)
-                if (dict.find(s.substr(j, i-j)) != dict.end() && dp[j])
+            tmp = q.front();
+            q.pop();
+            cur = tmp.first;
+            step = tmp.second;
+            if (cur == t)
+                return step;
+            for (int i = head[cur]; i != -1; i = edge[i].next)
+            {
+                int v = edge[i].t;
+                if (!visit[v])
                 {
-                    dp[i] = true;
-                    break;
+                    visit[v] = true;
+                    q.push(make_pair(v, step+1));
                 }
+            }
         }
-        return dp[s.size()];
+
+        return 0;
+    }
+
+    int ladderLength(string start, string end, unordered_set<string> &dict) {
+       if (dict.find(start) == dict.end())
+        dict.insert(start);
+       if (dict.find(end) == dict.end())
+        dict.insert(end);
+       unordered_map<string, int> lookup;
+       int i = 0, sid, eid;
+       int len = start.size();
+       memset(head, -1, sizeof(head));
+       memset(visit, false, sizeof(visit));
+       pos = 0;
+
+       for (unordered_set<string>::iterator iter = dict.begin(); iter != dict.end(); iter++)
+       {
+           lookup[*iter] = i;
+           if (*iter == start)
+            sid = i;
+            if (*iter == end)
+             eid = i;
+            str[i] = *iter;
+            i++;
+       }
+
+       for (int j = 0; j < i; j++)
+       for (int k = j+1; k < i; k++)
+            if(has_edge(str[j].c_str(), str[k].c_str(), len))
+            {
+                add_edge(k, j);
+                add_edge(j, k);
+            }
+       return bfs(sid, eid);
     }
 };
